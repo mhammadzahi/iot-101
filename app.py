77bd4d80e9b5bedc26b7e944041a4862c8cb10ac
Mohamed Zahi <mohamedzahi33@gmail.com>
2025-07-27 20:21:06 +0400

    start iiot dwh 2

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, Request
+import uvicorn
 
 
 app = FastAPI()
+
+@app.post("/api/v1/iot/101")
+async def iot_101(request: Request):
+    data = await request.json()
+    # Process the data as needed
+    return {"message": "Data received", "data": data}
+
 
 
 @app.get("/")
@@ -10,7 +18,5 @@ async def index(request: Request):
 
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run("app:app", host="127.0.0.1", port=8001, reload=True) # dev
     # uvicorn.run("app:app", host="0.0.0.0", port=8001) # prod
-
