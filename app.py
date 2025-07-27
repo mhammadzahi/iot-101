0b1556909a9834793691bd002db63b996bcfa4c6
Mohamed Zahi <mohamedzahi33@gmail.com>
2025-07-27 20:19:13 +0400

    start iiot dwh

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from pathlib import Path
-
-app = FastAPI(debug=True) # dev
-# app = FastAPI() # prod
 
 
-templates = Jinja2Templates(directory="templates")
+app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
+
+@app.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "name": "FastAPI User"})
-
-
+    return {"message": "Welcome to the IoT 101 API"}
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True) # dev
-    # uvicorn.run("app:app", host="0.0.0.0", port=8000) # prod
+    uvicorn.run("app:app", host="127.0.0.1", port=8001, reload=True) # dev
+    # uvicorn.run("app:app", host="0.0.0.0", port=8001) # prod
 
